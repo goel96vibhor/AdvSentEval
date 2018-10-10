9e45fa2af445645aa6a38bc5326accca0d41f65a
goelvibhor96 <vibhoriitr@gmail.com>
2018-10-10 02:00:44 -0500

    removed cuda for testing

--- examples/bow.py
+++ examples/bow.py
@@ -93,7 +93,7 @@ def batcher(params, batch):
 
 
 # Set params for SentEval
-params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 5}
+params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': False, 'kfold': 5}
 params_senteval['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
                                  'tenacity': 3, 'epoch_size': 2, 'cudaEfficient' : True}
 

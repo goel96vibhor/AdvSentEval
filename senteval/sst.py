5e7bfca316463914ce28bd4dfb47930046333df5
vgoelsj <vibhoriitr@gmail.com>
2018-11-25 02:26:06 -0600

    corrected percentage formula

--- senteval/sst.py
+++ senteval/sst.py
@@ -125,9 +125,9 @@ class SSTEval(object):
 
             sst_embed[key]['X'] = []
             for ii in range(0, len(self.sst_data[key]['y']), bsize):
-                n = len(self.sst_data[key]['y'])/10
-                if ii % n == 0:
-                    print("%d percent done out of %d"%( (ii*10 / n) , len(self.sst_data[key]['y'])))
+                n = len(self.sst_data[key]['y'])/bsize
+                if ((ii/bsize)*100/n) % 10 == 0:
+                    print("%d percent done out of %d"%( ((ii/bsize)*100/n), len(self.sst_data[key]['y'])))
                 batch = self.sst_data[key]['X'][ii:ii + bsize]
                 embeddings = batcher(params, batch)
                 sst_embed[key]['X'].append(embeddings)
